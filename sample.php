b3b02962c2e790b9fc57371b2ae138362c60ae06
itadmin <itadmin@lapr101.successivetech.com>
2019-10-10 15:36:44 +0530

    anothyer check

--- sample.php
+++ sample.php
@@ -9,13 +9,7 @@
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // collect value of input field
-    $name = $_REQUEST['fname'];
-    if (empty($name)) {
-        echo "Name is empty";
-    } else {
-        echo $name;
-    }
+    echo $_POST['fname'];
 }
 ?>
 
